8decf8cf1708e74bfc06cc972c0bc58d73597b99
AMRIT PRAJAPATI <110326844+Amritkp1211@users.noreply.github.com>
2026-03-16 10:32:07 +0000

    conversion operation

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,26 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//nan  for 123avb and also the 0 for null
+
+//true 1 and false 0
+
+let IsLoggedIn=1;
+
+let booleanLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanLoggedIn);
+
+//1 => true
+//  "" => false and
+//  "asnf" =>  true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
