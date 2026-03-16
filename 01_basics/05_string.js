292e829cc7a9d693b9722fd5f8d628ae8f0a3b8a
AMRIT PRAJAPATI <110326844+Amritkp1211@users.noreply.github.com>
2026-03-16 12:37:40 +0000

    num and math

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name ="hitesh";
+const repoCount=20;
+
+//console.log(name +repoCount +" done");
+
+console.log(`hellow my name is ${name }  and my repo count is ${repoCount}`);
+
+
--- 01_basics/dataypesSummary.js
+++ 01_basics/dataypesSummary.js
@@ -41,3 +41,4 @@ let user = {
     email :"user@mail.com",
     name :"kailash"
 }
+
