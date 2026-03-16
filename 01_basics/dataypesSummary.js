ea03e4aed68fc665e20f89b367930f06a6179387
AMRIT PRAJAPATI <110326844+Amritkp1211@users.noreply.github.com>
2026-03-16 11:21:54 +0000

    datatypes premit refrence

---
+++ 01_basics/dataypesSummary.js
@@ -0,0 +1,26 @@
+// #primitive datatypes
+
+//7 String,Number Boolean null undefined Symbol BigInt
+
+//dynamically types
+
+const score=100;
+const scoreValue=100.3
+
+const isLoggeIn = false;
+const outSideTemp = null
+let userEmail;
+
+const id =Symbol('123')
+const customerId =Symbol('123')
+
+console.log(id);
+console.log(customerId);
+
+console.log(id === customerId);
+
+
+
+//Refrence Type
+
+//Array ,Objects Funcntions 
