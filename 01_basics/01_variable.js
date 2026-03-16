33f49ba373bd81ffe7292abded29dd3fc19ae6f0
AMRIT PRAJAPATI <110326844+Amritkp1211@users.noreply.github.com>
2026-03-16 10:17:45 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId=218923; //locked value
+let accountEmail="sindrathsidrls@gmail.com"
+var accountPassword="shsfndffs"
+
+accountCity="Sirohi"
+// accountId=2; not allowed
+accountEmail="kp@gmail.com"
+accountPassword="ddaf"
+accountCity="kota"
+let accountState;
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+/*
+ prefer not to use var becaue 
+
+ because of issue of the block scope in the functional scope
+ 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+//number 
+//binint
+//string
+//boolean
+//undefined
+//null
+//symbol
+console.log(typeof null);
+console.log(typeof undefined)
